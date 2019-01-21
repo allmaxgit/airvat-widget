b18277ce9cf8cd808fd42035010d9f191dfd6e89
muerwre <gotham48@gmail.com>
2019-01-21 10:20:46 +0700

    added firebase

--- src/backend/js/routes/main/index.js
+++ src/backend/js/routes/main/index.js
@@ -1,3 +1,6 @@
+const firebase = require('@firebase/app').default;
+require('@firebase/firestore');
+
 module.exports = async (req, res) => {
   res.send({ success: true, message: 'Hello world!' });
 };
