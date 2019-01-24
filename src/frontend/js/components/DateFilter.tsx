d1aeeeba5888a18fc8518dfbbbb464a8c495d73b
muerwre <gotham48@gmail.com>
2019-01-24 09:39:41 +0700

    fixed date input

--- src/frontend/js/components/DateFilter.tsx
+++ src/frontend/js/components/DateFilter.tsx
@@ -1,25 +1,31 @@
 import * as React from 'react';
 import { SortButton } from "./SortButton";
+import { DateRangeInput } from "@blueprintjs/datetime";
+import * as moment from 'moment';
+
+import '@blueprintjs/core/lib/css/blueprint.css'
+import '@blueprintjs/datetime/lib/css/blueprint-datetime.css'
 
 type Props = {
-  lastActiveFrom: string,
-  lastActiveTo: string,
+  lastActiveFrom?: string,
+  lastActiveTo?: string,
   setFilter: (field: string, value: string) => void,
   sortOrder: string,
   onSortChange: (field: string) => void,
 }
 
 export class DateFilter extends React.Component<Props, {}> {
-  setActiveFrom = ({ target: { value } }: { target: { value: string }}): void => {
-    this.props.setFilter('lastActiveFrom', value);
-  };
-
-  setActiveTo = ({ target: { value } }: { target: { value: string }}): void => {
-    this.props.setFilter('lastActiveTo', value);
-  };
-
   changeSortOrder = (): void => {
     this.props.onSortChange('lastActive');
+  };
+
+  parseDateString = (date?: string): (Date | null) => (
+    (date && Date.parse(date) && new Date(date)) || null
+  );
+
+  setDateRange = ([ lastActiveFrom, lastActiveTo ]: [Date, Date]): void => {
+    if (lastActiveFrom) this.props.setFilter('lastActiveFrom', lastActiveFrom.toString());
+    if (lastActiveTo) this.props.setFilter('lastActiveTo', lastActiveTo.toString());
   };
 
   render() {
@@ -27,19 +33,14 @@ export class DateFilter extends React.Component<Props, {}> {
 
     return (
       <div>
-        <input
-          type="date"
-          max={lastActiveTo}
-          value={lastActiveFrom}
-          onChange={this.setActiveFrom}
-          size={1}
-        />
-        <input
-          type="date"
-          min={lastActiveFrom}
-          value={lastActiveTo}
-          onChange={this.setActiveTo}
-          size={1}
+        <DateRangeInput
+          formatDate={date => date.toLocaleDateString()}
+          onChange={this.setDateRange}
+          parseDate={str => new Date(str)}
+          value={[
+            this.parseDateString(lastActiveFrom),
+            this.parseDateString(lastActiveTo)
+          ]}
         />
         <SortButton sortOrder={sortOrder} onClick={this.changeSortOrder} />
       </div>
